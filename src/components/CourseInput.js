e655d359f127d4ca7ebf004c2e1c6a0d1ec7bb8e
peltonan <peltonen.annette@gmail.com>
2022-05-31 15:34:31 +0300

    Start of Section 6

---
+++ src/components/CourseInput.js
@@ -0,0 +1,28 @@
+import Button from './Button';
+import './CourseInput.css';
+import { useState } from 'react';
+
+const CourseInput = props => {
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const goalInputChangeHandler = event => {
+        setEnteredValue(event.target.value);
+    };
+
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        props.onAddGoal(enteredValue);
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label>Course Goal</label>
+                <input type="text" onChange={goalInputChangeHandler} />
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
+};
+
+export default CourseInput;
